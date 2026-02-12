296e9a51931463afc3402c2e3d3acd7081ab8615
Riccardo Condò <rcondo@studenti.apuliadigitalmaker.it>
2026-02-12 13:57:34 +0100

    .

--- pkg/utils/validators.py
+++ pkg/utils/validators.py
@@ -1,11 +1,11 @@
 from bson import ObjectId
 from fastapi import HTTPException
-from loguru import logger
+import logging
 
 
 def validate_object_id(id: str, field_name: str = "ID") -> ObjectId:
         try:
             return ObjectId(id)
         except Exception:
-            logger.warning(f"Invalid ObjectId format for {field_name}: {id}")
+            logging.warning(f"Invalid ObjectId format for {field_name}: {id}")
             raise HTTPException(status_code=400, detail=f"Invalid {field_name} format!")
