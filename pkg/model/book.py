fd659608b8184daad402114bad3904327f7861d2
Andrea_Petracca <andreapetracca155@gmail.com>
2026-05-14 17:16:00 +0200

    fix

--- pkg/model/book.py
+++ pkg/model/book.py
@@ -3,9 +3,10 @@ from bson import ObjectId
     
 @dataclass
 class Book:
+    isbn: str
     title: str
     genre: str
+    price: int
     author: str
-    isbn: str
     quotes: list[str] = field(default_factory=list)
     _id: ObjectId = field(default_factory = ObjectId) 
